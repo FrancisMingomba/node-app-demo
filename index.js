d71ef4e6862a795f4c9636c602fec1535295168a
Francis <mingomba@gmail.com>
2023-06-11 03:19:48 +0000

    wip

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const Joi = require('joi');
 const config = require('config');
+const Joi = require('joi');
 const express = require('express');
 const logger = require('./logger');
 const app = express();
 const createUser = require('./routes/createUser');
 const auth = require('./routes/auth');
+
+if(!config.get('jwtPrivateKey')){
+    console.error('FATAL ERROR: jwtPrivateKey is not define.');
+    process.exit(1);
+}
 
 app.use(express.json());
 app.use('/api/createUser', createUser);
